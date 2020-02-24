9387af5cf88ad14c1fe17f88641d83ec96805742
Seth Brown <seth.brown@ihsmarkit.com>
2020-02-24 13:16:35 -0700

    Mutation working + updating db file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 const { GraphQLServer } = require("graphql-yoga");
+const fs = require("fs");
 
 const users = require("./users.json");
-// 1
-const typeDefs = `
-type Query {
-  users: [User!]!,
-  usersById(id: Int!): [User!]!
-  usersByFavoriteFood(food: String): [User!]!
-}
 
-
-type User {
-	id: Int!,
-	firstName: String!,
-	lastName: String!,
-	favoriteFood: String,
-	favoriteRestaurant: String
-}
-`;
-
-// 2
 const resolvers = {
   Query: {
     users: () => users,
     usersById: (r, { id }, context, info) => users.filter(u => u.id === id),
     usersByFavoriteFood: (r, { food }, context, info) =>
       users.filter(u => u.favoriteFood === food)
+  },
+  Mutation: {
+    addUser: (r, args) => {
+      users.push({
+        id: args.id,
+        firstName: args.firstName,
+        lastName: args.lastName,
+        favoriteFood: args.favoriteFood,
+        favoriteRestaurant: args.favoriteRestaurant
+      });
+
+      fs.writeFileSync(
+        "./src/users.json",
+        JSON.stringify(users),
+        err => err && console.log(err)
+      );
+
+      return users.find(i => i.id === args.id);
+    }
   }
 };
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 
